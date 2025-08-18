53baa4fba06d4c18f83fa4050fbd9aff364734e1
Ayomide Ogunbode <richardogunbode@gmail.com>
2025-08-18 16:20:50 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant}>
+        <p className= {styles.sectionSubText}>Introduction</p>
+        <h2 className= {styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+
+    {/*About Me Section */}
+      <motion.p variants={fadeIn('up', 'spring', 0.2, 1)}
+        className= "mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        Hi👋 I’m Ayomide, a curious builder and problem-solver who enjoys turning ideas into simple, 
+        functional, and user-friendly digital experiences.
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+            <ServiceCard key={service.title } index={index} {...service} />
+        ))}
+      </div>
+
+    </>
   )
 }
 
